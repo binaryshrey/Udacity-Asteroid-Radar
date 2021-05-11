26c17aff92a046f2daa6297623447a05f4ba45d8
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2021-05-12 00:43:42 +0530

    adds databaseDao

---
+++ app/src/main/java/com/udacity/asteroidradar/database/DataBaseDao.kt
@@ -0,0 +1,23 @@
+package com.udacity.asteroidradar.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface DataBaseDao{
+
+    @Query("SELECT * FROM asteroids_table WHERE closeApproachDate >= DATE() ORDER BY closeApproachDate ASC")
+    fun getAsteroids(): LiveData<List<DataBaseEntity>>
+
+    @Query("SELECT * FROM asteroids_table ORDER BY closeApproachDate ASC")
+    fun getAllAsteroids(): LiveData<List<DataBaseEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(vararg asteroids: DataBaseEntity)
+
+    @Query("DELETE FROM asteroids_table WHERE closeApproachDate < DATE()")
+    suspend fun deleteStaleAsteroids()
+}
