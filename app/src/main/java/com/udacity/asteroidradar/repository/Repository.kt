b93b43d9633c1121ba8bab236c66a880bd4ee528
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2021-05-12 14:40:16 +0530

    adds repository

---
+++ app/src/main/java/com/udacity/asteroidradar/repository/Repository.kt
@@ -0,0 +1,35 @@
+package com.udacity.asteroidradar.repository
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.Constants
+import com.udacity.asteroidradar.api.*
+import com.udacity.asteroidradar.database.DatabaseAsteroid
+import com.udacity.asteroidradar.database.asDomainModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+
+class Repository(val databaseAsteroid: DatabaseAsteroid) {
+
+    val asteroids: LiveData<List<Asteroid>> = Transformations.map(
+        databaseAsteroid.dataBaseDao.getAsteroids()){
+        it.asDomainModel()
+    }
+
+    suspend fun refreshAsteroids(){
+        withContext(Dispatchers.IO){
+            try {
+                val asteroidDataString = APIService.retrofitService.getAsteroidData(getToday(),
+                    getSeventhDay(),Constants.API_KEY)
+                val asteroids = AsteroidContainer(parseAsteroidsJsonResult(JSONObject(asteroidDataString))).asDatabaseModel()
+                databaseAsteroid.dataBaseDao.insertAll(*asteroids)
+            } catch (e : Exception){
+                Log.i("Repository", "error : ${e.message}")
+            }
+        }
+    }
+
+}
